16e75239f4043c9cd62dc9233befb6cbb2b31b3a
KeeratiNon <keeratinon3@gmail.com>
2024-05-22 14:32:02 +0700

    skill-checkpoint

--- question-4.js
+++ question-4.js
@@ -4,4 +4,11 @@ const inventory = [
   { name: "Banana", price: 10, quantity: 50 },
   { name: "Orange", price: 30, quantity: 60 },
 ];
-// เริ่มเขียนโค้ดตรงนี้
+let minQuantity = inventory[0].quantity;
+
+for  (let i = 0; i < inventory.length; i++) {
+  if (inventory[i].quantity < minQuantity) {
+    minQuantity = inventory[i].quantity;
+    console.log (`สินค้าที่มีจำนวนต่ำที่สุดในคลังสินค้าคือ ${inventory[i].name} ซึ่งมี ${inventory[i].quantity} ชิ้น`)
+  }
+}
